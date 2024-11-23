56c8cf3fc4214b83b88f182b530c3cdfccf6ec0f
cbseirani <34148393+cbseirani@users.noreply.github.com>
2024-11-23 01:16:51 -0500

    add two numbers

--- program.py
+++ program.py
@@ -1,7 +1,12 @@
 from easy.two_sum import TwoSum
+from medium.add_two_numbers import AddTwoNumbers
 
 print("Leetcode solutions!")
 
+# 1. Two Sums
 TwoSum.run()
+
+# 2. Add Two Numbers
+AddTwoNumbers.run()
 
 print("Thanks!")
